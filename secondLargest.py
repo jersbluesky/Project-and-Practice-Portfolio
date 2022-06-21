1b95f4c420b387b1b44d3cd718201259170009fc
jersbluesky <joh02052@byui.edu>
2022-06-21 17:38:44 -0600

    SecondLargest problem finished

--- secondLargest.py
+++ secondLargest.py
@@ -1,5 +1,14 @@
-list = [12, 3, 5, 8, 90, 0]
+a_list = [12, 3, 5, 8, 90, 0]
+b_list = [1, 2, 3, 4, 5, 6, 3, 2, 1, 1, 1]
 
-list[3]
 
-print(list[3])
+def find_second_largest(list):
+    largest = list[0]
+    second_largest = None
+    for i in range(len(list)):
+        if list[i] > largest:
+            second_largest = largest
+            largest = list[i]
+    return second_largest
+        
+find_second_largest(a_list)
