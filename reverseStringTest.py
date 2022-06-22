ae9a45b22261598b35ac4a20d4ebdc649ac0fca9
jersbluesky <joh02052@byui.edu>
2022-06-21 18:27:25 -0600

    RST

--- reverseStringTest.py
+++ reverseStringTest.py
@@ -1,4 +1,4 @@
-def are_reverses(string_1, string_2):
+def are_reverses_1(string_1, string_2):
     reverse_string_1 = ""
     for i in range(len(string_1)):
         reverse_string_1 += string_1[-i-1]
@@ -9,5 +9,14 @@ def are_reverses(string_1, string_2):
         answer = False
     return answer
 
+
+def are_reverses_2(string_1, string_2):
+    for i in range(len(string_1)):
+        if string_1[i] == string_2[-i]:
+            return True
+        else:
+            return False
+
 print('Are "ABC" and "CBA" reverses of each other? (Should be True.)')
-are_reverses("ABC", "CBA")
+are_reverses_1("ABC", "CBA")
+are_reverses_2("ABC", "CBA")
