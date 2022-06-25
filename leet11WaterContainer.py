51b38c7e9515e3a10a6ea2a2294686ca628a98eb
jersbluesky <joh02052@byui.edu>
2022-06-24 22:05:31 -0600

    leet code problem 11 - Water container

---
+++ leet11WaterContainer.py
@@ -0,0 +1,36 @@
+class BruteForceSolution:
+    def maxArea(self, height) -> int:
+        currentMaxArea = 0
+        for i, value in enumerate(height):
+            for j, value in enumerate(height):
+                maxJ = 0
+                if j > maxJ and j > i:
+                    maxJ = j
+                    trueHeight = min(height[i],height[j])
+                    width = j - i
+                    area = trueHeight * width
+                    if area > currentMaxArea:
+                        currentMaxArea = area
+        return currentMaxArea
+    
+    
+class workingSolution:
+    def MaxArea(self, height) -> int:
+        
+        maxAreaResult = 0
+        
+        l, r = 0, len(height) - 1
+        while (l < r):
+            area = l * (r-l)
+            maxAreaResult = max(maxAreaResult, area)
+            if height[l] < height[r]:
+                l += 1
+            elif height[l] > height[r]:
+                r += 1
+            else:
+                l += 1
+            
+                
+            
+            
+            
