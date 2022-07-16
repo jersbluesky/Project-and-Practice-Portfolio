f31cae152c685a8118a4f18bb72c3efd5dd1c38d
jersbluesky <joh02052@byui.edu>
2022-07-16 12:49:29 -0600

    Simple probability program

---
+++ AncillaryMathAndPhysics/simpleProbability.py
@@ -0,0 +1,43 @@
+# Simple Probability: written with objects
+# Author: Jeremy Johnson
+# Date: 7/16/2022
+
+# If a number is randomly chosen from the following list, what is the
+# probability that the number is a multiple of any given input?
+
+import time
+import numpy as np
+
+class Percent(float):
+    def __str__(self):
+        return '{:.2%}'.format(self)
+
+class DivisibleBy:
+    def __init__(self, prob_list_1, chosen_int) -> None:
+            
+        self._prob_list_1 = prob_list_1
+        self._chosen_int = chosen_int
+    
+    def probability(self):
+        nS = len(self._prob_list_1)
+        nA = 0
+        for i, value in enumerate(self._prob_list_1):
+            if self._prob_list_1[i] % self._chosen_int == 0:
+                nA += 1
+        probability = Percent(nA / nS)
+        print(probability)        
+    
+size = np.random.randint(10000, 1000000)
+print(size)
+prob_list_1= np.random.randint(1,100000,size)
+
+chosen_int = int(input("Choose and integer: "))
+
+startTime = time.time()
+testCaseOne = DivisibleBy(prob_list_1, chosen_int)
+testCaseOneAnswer = testCaseOne.probability()
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))   
+            
+        
