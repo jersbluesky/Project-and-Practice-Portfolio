5e44f4f8c92786b13b8e6ed9e816d211e66ef353
jersbluesky <joh02052@byui.edu>
2022-07-17 12:28:11 -0600

    Made it more customizable for testing purposes

--- AncillaryMathAndPhysics/tools/sampleListInt.py
+++ AncillaryMathAndPhysics/tools/sampleListInt.py
@@ -1,6 +1,5 @@
-# This tool creates a List Array of a sample size provided by 
-# the user. The size of the array is randomly set. It could 
-# populate an empty array, and can output duplicate integers.
+# This tool creates a List Array of random integers. The size, 
+# minimum, and maximum integers are chosen by the user.
 
 # Author: Jeremy Johnson
 # Date: 7-17-2022
@@ -12,13 +11,20 @@ class ListArray:
     def __init__(self):
         self._template_array = []
         
-    def sample_size(self):
-        self._sample_size = int(input("Sample Size: "))
-        return self._sample_size
+    def get_array_size(self):
+        self._array_size = int(input("Array size: "))
+        return self._array_size
+    
+    def get_max_int(self):
+        self._max_int = int(input("Max integer: "))
+        return self._max_int
+    
+    def get_min_int(self):
+        self._min_int = int(input("Min integer: "))
+        return self._min_int
         
     def set_list_array(self):
-        array_size = np.random.randint(0, self._sample_size)
-        numpy_array = np.random.randint(0, self._sample_size, array_size)
+        numpy_array = np.random.randint(self._min_int, self._max_int, self._array_size)
         self._template_array = list(numpy_array)
 
     def get_list_array(self):
@@ -26,12 +32,14 @@ class ListArray:
     
     def execute(self): 
         array = ListArray()
-        size = array.sample_size()
+        size = array.get_array_size()
+        minInt = array.get_min_int()
+        maxInt = array.get_max_int()
         list = array.set_list_array()
         return array.get_list_array()
 
 # TO CALL:
-# createArray = ListArray()
-# testList = createArray.execute()
-# # To test:
-# print(testList)
+createArray = ListArray()
+testList = createArray.execute()
+# To test:
+print(testList)
